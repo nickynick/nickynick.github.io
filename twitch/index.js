ea66e4b703ffd15a4348feef04aba664a6338f51
Nick Tymchenko <t.nick.a@gmail.com>
2016-04-24 19:01:30 +0300

    I hate this

--- twitch/index.js
+++ twitch/index.js
@@ -2,9 +2,10 @@ window.CLIENT_ID = 'f51444r0qklip2pyna1yp8x3byctadw';
 
 function refreshStreams() {
   Twitch.api({ method: 'streams/followed', params: { limit: 100, stream_type: 'live' } }, function(error, result) {
-    console.log(result);
-
     for (stream in result.streams) {
+      console.log(stream);
+      console.log(stream.channel);
+
       var streamListElement = document.getElementById('stream-list')
       while (streamListElement.firstChild) {
         streamListElement.removeChild(streamListElement.firstChild);
