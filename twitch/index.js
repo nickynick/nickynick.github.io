932729f66f556322004ca288d6a05d8bbe54bdea
Nick Tymchenko <t.nick.a@gmail.com>
2016-04-24 18:34:31 +0300

    Use twitch example code

--- twitch/index.js
+++ twitch/index.js
@@ -1,14 +1,45 @@
-Twitch.init({clientId: 'f51444r0qklip2pyna1yp8x3byctadw'}, function(error, status) {
-    if (status.authenticated) {
-        // Already logged in, hide button
-        $('.twitch-connect').hide()
-    }
-});
+window.CLIENT_ID = 'f51444r0qklip2pyna1yp8x3byctadw';
 
-window.addEventListener("DOMContentLoaded", function() {    
-    $('.twitch-connect').click(function() {
-        Twitch.login({
-            scope: ['user_read', 'channel_read']
-        });
+$(function() {
+  // Initialize. If we are already logged in, there is no
+  // need for the connect button
+  Twitch.init({clientId: CLIENT_ID}, function(error, status) {
+    if (status.authenticated) {
+      // we're logged in :)
+      $('.status input').val('Logged in! Allowed scope: ' + status.scope);
+      // Show the data for logged-in users
+      $('.authenticated').removeClass('hidden');
+    } else {
+      $('.status input').val('Not Logged in! Better connect with Twitch!');
+      // Show the twitch connect button
+      $('.authenticate').removeClass('hidden');
+    }
+  });
+
+
+  $('.twitch-connect').click(function() {
+    Twitch.login({
+      scope: ['user_read', 'channel_read']
     });
+  })
+
+  $('#logout button').click(function() {
+    Twitch.logout();
+
+    // Reload page and reset url hash. You shouldn't
+    // need to do this.
+    window.location = window.location.pathname
+  })
+
+  $('#get-name button').click(function() {
+    Twitch.api({method: 'user'}, function(error, user) {
+      $('#get-name input').val(user.display_name);
+    });
+  })
+
+  $('#get-stream-key button').click(function() {
+    Twitch.api({method: 'channel'}, function(error, channel) {
+      $('#get-stream-key input').val(channel.stream_key);
+    });
+  })
 });
