6345a0e507888a2e656c9091995be004819ec2ad
Nick Tymchenko <t.nick.a@gmail.com>
2016-04-24 18:56:08 +0300

    Get stream list

--- twitch/index.js
+++ twitch/index.js
@@ -1,14 +1,32 @@
 window.CLIENT_ID = 'f51444r0qklip2pyna1yp8x3byctadw';
 
+function refreshStreams() {
+  Twitch.api({ method: 'streams/followed', params: { limit: 100, stream_type: 'live' } }, function(error, result) {
+    for (stream in result.streams) {
+      var streamListElement = document.getElementById('stream-list')
+      while (streamListElement.firstChild) {
+        streamListElement.removeChild(streamListElement.firstChild);
+      }
+
+      var streamElement = document.createElement('li');
+      streamElement.innerHTML = '<a href="html5player.html?channel=' + stream.channel.name + '">' + stream.channel.display_name + '</a>';
+
+      streamListElement.appendChild(streamElement);
+    }    
+  });
+}
+
 $(function() {
   // Initialize. If we are already logged in, there is no
   // need for the connect button
   Twitch.init({clientId: CLIENT_ID}, function(error, status) {
-    if (status.authenticated) {
+    if (status.authenticated) {      
       // we're logged in :)
       $('.status input').val('Logged in! Allowed scope: ' + status.scope);
       // Show the data for logged-in users
       $('.authenticated').removeClass('hidden');
+
+      refreshStreams()
     } else {
       $('.status input').val('Not Logged in! Better connect with Twitch!');
       // Show the twitch connect button
@@ -26,20 +44,7 @@ $(function() {
   $('#logout button').click(function() {
     Twitch.logout();
 
-    // Reload page and reset url hash. You shouldn't
-    // need to do this.
+    // Reload page and reset url hash. You shouldn't need to do this.
     window.location = window.location.pathname
-  })
-
-  $('#get-name button').click(function() {
-    Twitch.api({method: 'user'}, function(error, user) {
-      $('#get-name input').val(user.display_name);
-    });
-  })
-
-  $('#get-streams').click(function() {
-    Twitch.api({ method: 'streams/followed', params: { limit: 100, stream_type: 'live' } }, function(error, result) {
-      $('#get-streams-result').html(result.streams);
-    });
   })
 });
